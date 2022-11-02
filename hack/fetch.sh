082aff8598e999b27cb7698ffc7a9b2e10a26e85
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2022-11-02 20:37:28 +0000

    build: flatten chart structure
    Signed-off-by: Deavon M. McCaffery <dmccaffery@users.noreply.github.com>

--- hack/fetch.sh
+++ hack/fetch.sh
@@ -17,50 +17,45 @@ if ! command -v helm 1>/dev/null 2>&1; then
     curl -L https://git.io/get_helm.sh | bash
 fi
 
+rm -rf ./charts/*
+
 # cert manager
-rm -rf ./charts/jetstack/*
 helm repo add jetstack https://charts.jetstack.io
-helm fetch --untar --untardir ./charts/jetstack jetstack/cert-manager
-helm fetch --untar --untardir ./charts/jetstack jetstack/cert-manager-istio-csr
-helm fetch --untar --untardir ./charts/jetstack jetstack/cert-manager-csi-driver
-helm fetch --untar --untardir ./charts/jetstack jetstack/cert-manager-approver-policy
+helm fetch --untar --untardir ./charts jetstack/cert-manager
+helm fetch --untar --untardir ./charts jetstack/cert-manager-istio-csr
+helm fetch --untar --untardir ./charts jetstack/cert-manager-csi-driver
+helm fetch --untar --untardir ./charts jetstack/cert-manager-approver-policy
 
 # prometheus
-rm -rf ./charts/prometheus/*
 helm repo add prometheus https://prometheus-community.github.io/helm-charts
-helm fetch --untar --untardir ./charts/prometheus prometheus/prometheus
-helm fetch --untar --untardir ./charts/prometheus prometheus/prometheus-elasticsearch-exporter
+helm fetch --untar --untardir ./charts prometheus/prometheus
+helm fetch --untar --untardir ./charts prometheus/prometheus-elasticsearch-exporter
 
 # grafana
-rm -rf ./charts/grafana/*
 helm repo add grafana https://grafana.github.io/helm-charts
-helm fetch --untar --untardir ./charts/grafana grafana/grafana
+helm fetch --untar --untardir ./charts grafana/grafana
 
 # istio
-rm -rf ./charts/istio/*
 helm repo add istio https://istio-release.storage.googleapis.com/charts
-helm fetch --untar --untardir ./charts/istio istio/base
-helm fetch --untar --untardir ./charts/istio istio/istiod
-helm fetch --untar --untardir ./charts/istio istio/gateway
-helm fetch --untar --untardir ./charts/istio istio/cni
+helm fetch --untar --untardir ./charts istio/base
+helm fetch --untar --untardir ./charts istio/istiod
+helm fetch --untar --untardir ./charts istio/gateway
+helm fetch --untar --untardir ./charts istio/cni
 
 # elastic
-rm -rf ./charts/elastic/*
 helm repo add elastic https://helm.elastic.co
-helm fetch --untar --untardir ./charts/elastic elastic/elasticsearch
-helm fetch --untar --untardir ./charts/elastic elastic/kibana
+helm fetch --untar --untardir ./charts elastic/elasticsearch
+helm fetch --untar --untardir ./charts elastic/kibana
 
 # flux
-rm -rf ./charts/flux/*
 helm repo add flux https://fluxcd-community.github.io/helm-charts
-helm fetch --untar --untardir ./charts/flux flux/flux2
-helm fetch --untar --untardir ./charts/flux flux/flux2-notification
-helm fetch --untar --untardir ./charts/flux flux/flux2-sync
+helm fetch --untar --untardir ./charts flux/flux2
+helm fetch --untar --untardir ./charts flux/flux2-notification
+helm fetch --untar --untardir ./charts flux/flux2-sync
 
 # flagger
-rm -rf ./charts/flagger/*
 helm repo add flagger https://flagger.app
-helm fetch --untar --untardir ./charts/flagger flagger/flagger
+helm fetch --untar --untardir ./charts flagger/flagger
 
 # find charts -depth 2 -min -name 'Chart.yaml' -print0 | \
 #     xargs -0 -I {} echo helm template "$(basename {})"
